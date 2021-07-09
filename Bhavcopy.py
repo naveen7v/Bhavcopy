54bed7c3f09e2b6420608171fd8ed481835393f2
Naveen <33922357+naveen7v@users.noreply.github.com>
2021-07-09 20:14:54 +0530

    Update Bhavcopy.py
    Corrected import stmts as per PEP-8 and added spaces at logical blocks/sections for code readability

--- Bhavcopy.py
+++ Bhavcopy.py
@@ -1,10 +1,18 @@
-import requests, zipfile, os, io, pandas as pd
+
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
+import  zipfile, os, io
+
+import requests
+import pandas as pd
+
 base = '/path/to/download/'
 t = datetime.today().date()
-dmonth={'01':'JAN','02':'FEB','03':'MAR','04':'APR','05':'MAY','06':'JUN','07':'JUL','08':'AUG','09':'SEP','10':'OCT','11':'NOV','12':'DEC'}
+
+dmonth = {'01':'JAN','02':'FEB','03':'MAR','04':'APR','05':'MAY','06':'JUN',
+          '07':'JUL','08':'AUG','09':'SEP','10':'OCT','11':'NOV','12':'DEC'}
+
 
 # Before running this script , create a file called log.txt and write the date from which you want to download EOD data
 # Opening file named log.txt , which keeps track of the last downloaded date.
@@ -23,6 +31,7 @@ for i in range(1,diff.days+1):
         os.mkdir(base+y+'/Futures')
     zpath = base+y+'/'+d+'.zip'
     
+    
     for i in range(7):
         while True:
             try:
@@ -31,17 +40,21 @@ for i in range(1,diff.days+1):
                 print('No connection, retrying')
             break
             
+            
     if a.status_code==200:
         dload=open(zpath, 'wb')
         dload.write(a.content)
         dload.close()
+        
         z = zipfile.ZipFile(zpath, 'r')
         z.extractall(base+y+'/')
         z.close()
         os.remove(zpath)
+        
         f, deldict = pd.read_csv(base+y+'/cm'+d+dmonth[m]+y+'bhav.csv'), {}  #reading the raw dl-ed bhav file
         f = f[f['SERIES'] == 'EQ'] #retaining only EQ rows and leaving out bonds,options etc
         deliverable = requests.get('https://nseindia.com/archives/equities/mto/MTO_'+d+m+y+'.DAT').text.splitlines()
+        
         del deliverable[:4]
 
         for i in deliverable:
@@ -60,18 +73,22 @@ for i in range(1,diff.days+1):
             indx.to_csv(base+y+'/Index/Indices'+ str(nextdt.date())+'.csv', index=False)
             indx[['Index Name', 'Index Date', 'Open Index Value', 'High Index Value', 'Low Index Value', 'Closing Index Value', 'Volume']]
             
-            indx = indx.rename(columns={'Index Name' : 'SYMBOL', 'Index Date' : 'TIMESTAMP', 'Open Index Value' : 'OPEN', 'High Index Value' : 'HIGH',
-                                        'Low Index Value' : 'LOW', 'Closing Index Value' : 'CLOSE', 'Volume' : 'TOTTRDQTY'})
+            indx = indx.rename(columns={'Index Name' : 'SYMBOL', 'Index Date' : 'TIMESTAMP', 'Open Index Value' : 'OPEN', 
+                                        'High Index Value' : 'HIGH', 'Low Index Value' : 'LOW', 'Closing Index Value' : 'CLOSE', 
+                                        'Volume' : 'TOTTRDQTY'})
+            
             f=f.append(indx, ignore_index=True)
         
         f['TIMESTAMP'] = pd.Series(str(nextdt.date().strftime('%Y%m%d')) for _ in range(len(f)))
         f = f[['SYMBOL', 'TIMESTAMP', 'OPEN', 'HIGH', 'LOW', 'CLOSE', 'TOTTRDQTY', 'DELIVERABLE']]
         f.to_csv(base+y+'/'+str(nextdt.date())+'.csv', index=False)
         os.remove(base+y+'/cm'+d+dmonth[m]+y+'bhav.csv')
+        
         futures = requests.get('https://nseindia.com/content/historical/DERIVATIVES/'+y+'/'+dmonth[m]+'/fo'+d+dmonth[m]+y+'bhav.csv.zip')
         fo = open(zpath, 'wb')
         fo.write(futures.content)
         fo.close()
+        
         z, wr = zipfile.ZipFile(zpath,'r'), nextdt.date()
         z.extractall(base+y+'/Futures')
         z.close()
